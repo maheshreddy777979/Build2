9eab8ce042977fed89e152a2ae4529a6fbde1b11
maheshreddy777979 <maheshpolimeru777@gmail.com>
2026-01-12 19:22:04 +0530

    Update print statement for webhook test

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "another test commit for webhooks");
+        System.out.println( "another test git commit for webhook ");
     }
 }
